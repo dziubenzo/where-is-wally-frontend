b1222a6a9f2f3492d16eb8b55b57e26ef8e99d77
dziubenzo <dziubenzo@gmail.com>
2024-04-02 19:35:36 +0200

    Write page layout CSS

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -5,8 +5,29 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    background-color: ${({ theme }) => theme.colours.background};
-    color: ${({ theme }) => theme.colours.primary};
+    font-family: "Poppins", sans-serif;
+    font-weight: 400;
+    font-style: normal;
+    background-color: ${(props) => props.theme.colours.background};
+    color: ${(props) => props.theme.colours.primary};
+  }
+
+  *:focus-visible {
+    outline: 3px solid ${(props) => props.theme.colours.primary}  ;
+  }
+
+  body {
+    display: flex;
+    justify-content: center;
+  }
+
+  #root {
+    display: grid;
+    grid-template-rows: min-content auto min-content;
+    place-items: center;
+    min-height: 100svh;
+    max-width: 1200px;
+    padding: 0 1em 1em 1em;
   }
 `;
 
