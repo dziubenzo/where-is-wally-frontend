a5356d188a7e42b12c5f2efa6caa3998ef9f54fd
dziubenzo <dziubenzo@gmail.com>
2024-04-05 11:54:34 +0200

    Use fetched data in HomePage component

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,14 +1,26 @@
 import { StyledHomePage } from '../styles/HomePage.styled';
+import { useLoaderData, useNavigation } from 'react-router-dom';
 
 function HomePage() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === 'loading';
+
+  const { levelsCount, playersCount, latestPlayer } = useLoaderData();
+
   return (
     <StyledHomePage>
-      <h2>Levels:</h2>
-      <h1>TODO</h1>
-      <h2>Leaderboard Entries:</h2>
-      <h1>TODO</h1>
-      <h2>Latest Entry:</h2>
-      <h1>TODO</h1>
+      {isLoading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <>
+          <h2>Levels:</h2>
+          <h1>{levelsCount}</h1>
+          <h2>Leaderboard Entries:</h2>
+          <h1>{playersCount}</h1>
+          <h2>Latest Entry:</h2>
+          <h1>{latestPlayer ? latestPlayer.nickname : 'No players yet :('}</h1>
+        </>
+      )}
     </StyledHomePage>
   );
 }
