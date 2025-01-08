12785ce14227abb31f1c98f515eea878f3a29b73
dziubenzo <dziubenzo@gmail.com>
2025-01-08 11:23:08 +0100

    Make API URL environment-agnostic

--- src/API.ts
+++ src/API.ts
@@ -1,4 +1,5 @@
-const API_URL = 'https://where-is-wally-backend.fly.dev';
-// const API_URL = 'http://192.168.0.13:3000';
+const API_URL = import.meta.env.DEV
+  ? 'http://192.168.0.13:3000'
+  : 'https://where-is-wally-backend.fly.dev';
 
 export default API_URL;
